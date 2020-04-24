11963673d98f9541e60d1f28b28fb129c4d6ebaa
Ritukuklani <rtkuklani@gmail.com>
2020-04-23 21:19:33 -0400

    Authentication webpage

--- AWS Assign 2/FrontEnd/Webpage WP2/approveJS.js
+++ AWS Assign 2/FrontEnd/Webpage WP2/approveJS.js
@@ -3,16 +3,6 @@ var chatHistory = [];
 var apigClient = null;
 // const AWS =require('aws-sdk');
 var url_string = window.location.href;
-// var cognito_token = url_string.substring(url_string.indexOf("=") + 1,url_string.indexOf("&"));
-
-// AWS.config.region = 'us-east-1';
-// AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-//   IdentityPoolId: 'Your Identity Pool ID',
-// 	Logins: {
-// 	   'cognito-idp.us-east-1.amazonaws.com/us-east-1_8MsV07uIJ': cognito_token
-// 	}
-// });
-
 function callInputLambda() {
   var itext = document.getElementById('name-text').value.trim()
   // var inputText = itext.toLowerCase();
@@ -22,18 +12,20 @@ function callInputLambda() {
     return false;
   }
   else {
-    chatHistory.push("User: " + itext);
+    // chatHistory.push("User: " + itext);
     // chatHistory.push("User: " + ptext);
-  //   document.getElementById('chat-output').innerHTML = "";
-  //   chatHistory.forEach((element) => {
-  //     document.getElementById('chat-output').innerHTML += "<p>" + element + "</p>";
+    // document.getElementById('chat-output').innerHTML = "";
+    // chatHistory.forEach((element) => {
+    //   document.getElementById('chat-output').innerHTML += "<p>" + element + "</p>";
      };
+     console.log(itext);
   setTimeout(chatbotResponse, 500, itext);
   
   return false;
   
   //return chatbotResponse(inputText,phonenumberText);
 }
+
 
 function chatbotResponse(itext) {
   // return AWS.config.credentials.getPromise()
@@ -59,18 +51,101 @@ function chatbotResponse(itext) {
     console.log(body)
     return apigClient.validateOTPPost(params,body,additionalParams)
   // // })
-  //  .then((result) =>{
-      
-  //     chatHistory.push("Bot: " + result.message);
-  //     document.getElementById('chat-output').innerHTML = "";
-  //     console.log(result.message)
-  //     chatHistory.forEach((element) => {
-  //       document.getElementById('chat-output').innerHTML += "<p>" + element + "</p>";
-  //     });
-  // })
-  // .catch((err) =>{
-  //   console.log(err);
-  // });
+   .then((result) =>{
+    r1 = result["data"];
+    console.log(r1);
+    // r2 = JSON.stringify(r1);
+    // r3 = r2.substring(3, r2.length-3);
+    if (r1=='false'){
+      chatHistory.push("you can not enter through the virtual door");
+    }
+    else{
+      chatHistory.push(r1 + " you can enter through the virtual door");
+    }
+   
+      document.getElementById('chat-output').innerHTML = "";
+      // console.log(result.message)
+      chatHistory.forEach((element) => {
+        document.getElementById('chat-output').innerHTML += "<p>" + element + "</p>";
+      });
+  })
+  .catch((err) =>{
+    console.log(err);
+  });
  }
+
+
+
+
+
+
+
+
+
+
+
+// var chatHistory = [];
+// var apigClient = null;
+// // const AWS =require('aws-sdk');
+// var url_string = window.location.href;
+// function callInputLambda() {
+//   var itext = document.getElementById('name-text').value.trim()
+//   // var inputText = itext.toLowerCase();
+//   document.getElementById('name-text').value = "";
+//   if(itext == "") {
+//     alert("Sorry! You entered wrong OTP.");
+//     return false;
+//   }
+//   else {
+//     chatHistory.push("User: " + itext);
+//     // chatHistory.push("User: " + ptext);
+//   //   document.getElementById('chat-output').innerHTML = "";
+//   //   chatHistory.forEach((element) => {
+//   //     document.getElementById('chat-output').innerHTML += "<p>" + element + "</p>";
+//      };
+//   setTimeout(chatbotResponse, 500, itext);
+  
+//   return false;
+  
+//   //return chatbotResponse(inputText,phonenumberText);
+// }
+
+// function chatbotResponse(itext) {
+//   // return AWS.config.credentials.getPromise()
+//   // .then(()=>{
+//   //   console.log('Successfully logged!');
+//   apigClient = apigClientFactory.newClient({
+//   //     accessKey: AWS.config.credentials.accessKeyId,
+//   //     secretKey: AWS.config.credentials.secretAccessKey,
+//   //     // sessionToken: AWS.config.credentials.sessionToken
+//      });
+//     var params = {
+      
+//     };
+//     var body = {
+//       "otp":itext
+//     };
+//     var additionalParams = {
+//       // headers: {
+//       //   'x-api-key': 'Your API KEY'
+//       // },
+//       // queryParams: {}
+//     };
+//     console.log(body)
+//     return apigClient.validateOTPPost(params,body,additionalParams)
+//   // // })
+//   //  .then((result) =>{
+      
+//   //     chatHistory.push("Bot: " + result.message);
+//   //     document.getElementById('chat-output').innerHTML = "";
+//   //     console.log(result.message)
+//   //     chatHistory.forEach((element) => {
+//   //       document.getElementById('chat-output').innerHTML += "<p>" + element + "</p>";
+//   //     });
+//   // })
+//   // .catch((err) =>{
+//   //   console.log(err);
+//   // });
+//  }
 
 
